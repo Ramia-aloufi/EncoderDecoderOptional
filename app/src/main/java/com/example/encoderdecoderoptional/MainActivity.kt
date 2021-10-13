2a747ca153fbec53c1e21665dbf2572731908285
RAM111111 <r.r.99@windowslive.com>
2021-10-13 19:50:43 +0300

    first commit

---
+++ app/src/main/java/com/example/encoderdecoderoptional/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.encoderdecoderoptional
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
